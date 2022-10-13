e3b3d29d520c8f26a75bdcbd1822014117e5da15
DavidP1996erea <david.perea@iesnervion.es>
2022-10-13 18:27:05 +0200

    todo acabado

--- app/src/main/java/com/example/primeraaplicacion/alinearTexto.kt
+++ app/src/main/java/com/example/primeraaplicacion/alinearTexto.kt
@@ -12,6 +12,11 @@ class alinearTexto : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alinear_texto)
         supportActionBar?.hide()
+
+        var texto = findViewById<TextView>(R.id.textoAlinear)
+
+        texto.gravity = Gravity.CENTER_HORIZONTAL
+
     }
 
 
